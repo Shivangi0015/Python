351a2fd15ab3b6b74a23a118720647f9f6102065
Shivangi0015 <shivangirawat2309@gmail.com>
2025-03-24 23:15:23 +0530

    Initial commit

---
+++ py_basics.py
@@ -0,0 +1,28 @@
+#These are the basics of python
+#DAY 1 program 1
+#VS code me koi bhi file .py se save karna nhi to wo normal file samjhega
+
+print(1)
+print("the above sentence is not a string hence it can be written without the double quotes")
+print( )
+
+print("hello")
+print("the above sentence is a string and hence it needs to be written inside double quotes else it will show error")
+print( )
+
+#printing result directly in code
+print(5+3)
+print("Added 5 and 3")
+print( )
+
+#printing result
+print("5+3")
+print("python will consider the above equation as string and display it as it is\n")
+
+#new line in python
+print("\\n")
+print("The above statement is used for a new line just like the one in C programming\n")
+
+#printing multiple values in single line 
+print("hello",5,6)
+print("We can print multiple values in single line using just a comma\n")
